f8f1c623c42bdd66895868ebc72153b9cebba7af
Aleff <aleffbruno@gmail.com>
2019-11-13 22:49:51 -0300

    add templateCartPage, fakeServer, get itens using API

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,5 +1,63 @@
 import React from 'react';
+import { MdRemoveCircleOutline, MdAddCircleOutline, MdDelete } from 'react-icons/md';
+
+import { Container, ProductTable, Total } from './styles';
 
 export default function Cart() {
-    return <h1>Cart</h1>
+    return (
+        <Container>
+            <ProductTable>
+                <thead>
+                    <tr>
+                        <th></th>
+                        <th>PRODUTO</th>
+                        <th>QTD</th>
+                        <th>SUBTOTAL</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>
+                            <img src="https://static.netshoes.com.br/produtos/tenis-adidas-duramo-lite-2-0-masculino/28/COL-3586-128/COL-3586-128_detalhe2.jpg?ims=326x" alt="Tenis" />
+                        </td>
+                        <td>
+                            <strong>Tenis bonito demais</strong>
+                            <span>R$199,99</span>
+                        </td>
+                        <td>
+                            <div>
+                                <button type="button">
+                                    <MdRemoveCircleOutline size="20" color="#7159c1" />
+                                </button>
+                                <input type="number" readOnly value={2} />
+                                <button type="button">
+                                    <MdAddCircleOutline size="20" color="#7159c1" />
+                                </button>
+                            </div>
+                        </td>
+                        <td>
+                            <strong>R$399,98</strong>
+                        </td>
+                        <td>
+                            <button type="button">
+                                <MdDelete size={20} color="#7159c1"/>
+                            </button>
+                        </td>
+                    </tr>
+                </tbody>
+            </ProductTable>
+
+            <footer>
+                <button type="button">
+                    Finalizar Pedido
+                </button>
+
+                <Total>
+                    <span>TOTAL</span>
+                    <strong>R$1990,22</strong>
+                </Total>
+            </footer>
+        </Container>
+    )
 }
