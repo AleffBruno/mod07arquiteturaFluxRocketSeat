7cc1e594971def6da5f0c5f1ba32733d2000a6a9
Aleff <aleffbruno@gmail.com>
2019-11-15 18:59:25 -0300

    first interaction with actions reducers and redux

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { MdAddShoppingCart } from 'react-icons/md';
 import { formatPrice } from '../../util/format';
 import api from '../../services/api';
 
 import { ProductList } from './styles';
 
-export default class Home extends Component {
+class Home extends Component {
     state = {
         products: [],
     };
@@ -23,17 +24,26 @@ export default class Home extends Component {
         })
     }
 
+    handleAddProduct = product => {
+        const { dispatch } = this.props;
+
+        dispatch({
+            type: 'ADD_TO_CART',
+            product
+        });
+    }
+
     render() {
         const { products } = this.state;
         return (
             <ProductList>
                 { products.map(product => (
                     <li key={product.id}>
-                        <img src={product.image} alt={product.title} />
+                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTeWRbf3__UtMdIB8nHgrHtiybeKcdP26TeFFgVL6Up7Oh_GVJu" alt={product.title} />
                         <strong>{product.title}</strong>
                         <span>{product.priceFormatted}</span>
         
-                        <button type="button">
+                        <button type="button" onClick={() => this.handleAddProduct(product)}>
                             <div>
                                 <MdAddShoppingCart size={16} color="#fff" /> 3
                             </div>
@@ -47,3 +57,5 @@ export default class Home extends Component {
         )
     }
 }
+
+export default connect()(Home);
